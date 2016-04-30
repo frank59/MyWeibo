339469c9a59afca76300e8eb3dfaba83b05dcb4a
frank59 <380330439@qq.com>
2016-04-30 19:39:23 +0800

    test01

---
+++ app/src/main/java/com/geewaza/android/myweibo/TestActivity.java
@@ -0,0 +1,16 @@
+package com.geewaza.android.myweibo;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by wangh on 2016/4/30.
+ */
+public class TestActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.test_activity);
+	}
+}
